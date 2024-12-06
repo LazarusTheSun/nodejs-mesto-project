255a641d3442852fd76f5c45ac70347cfb01fa02
Kirill Evrov <kirillevrov@gmail.com>
2024-12-06 20:01:41 +0300

    add user and card models

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose'
 
 const { PORT = 3000 } = process.env;
 
+const MONGO_URL = 'mongodb://localhost:27017/mestodb ';
+const DB_NAME = 'mestodb';
+
 const app = express();
+
+mongoose.connect(`${MONGO_URL}/${DB_NAME}`);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ src/models/card.ts
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+
+interface ICard {
+  name: string;
+  link: string;
+  owner: Schema.Types.ObjectId;
+  likes: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
+
+const CardSchema = new Schema<ICard>({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: [Schema.Types.ObjectId],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+export default model<ICard>('card', CardSchema);
